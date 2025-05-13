6b12bcfece45dfe5d640217b6930bbad08bcf6ae
JKLeorio <jkdplatinum@gmail.com>
2025-05-14 01:40:59 +0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace RPNCalculator
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtRPN;
+        private System.Windows.Forms.Button btnAddVariable;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.FlowLayoutPanel panelVariables;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPageCalc;
+        private System.Windows.Forms.TabPage tabPageConvert;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtRPNResult;
+        private System.Windows.Forms.TextBox txtInfix;
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPageCalc = new System.Windows.Forms.TabPage();
+            this.tabPageConvert = new System.Windows.Forms.TabPage();
+
+            this.txtRPN = new System.Windows.Forms.TextBox();
+            this.btnAddVariable = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.panelVariables = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtResult = new System.Windows.Forms.TextBox();
+
+            this.txtInfix = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtRPNResult = new System.Windows.Forms.TextBox();
+
+            this.tabControl.SuspendLayout();
+            this.tabPageCalc.SuspendLayout();
+            this.tabPageConvert.SuspendLayout();
+            this.SuspendLayout();
+
+            
+            this.tabControl.Controls.Add(this.tabPageCalc);
+            this.tabControl.Controls.Add(this.tabPageConvert);
+            this.tabControl.Location = new System.Drawing.Point(10, 10);
+            this.tabControl.Size = new System.Drawing.Size(620, 450);
+
+            
+            this.txtRPN.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtRPN.Location = new System.Drawing.Point(20, 20);
+            this.txtRPN.Size = new System.Drawing.Size(500, 29);
+            this.txtRPN.PlaceholderText = "Введите RPN выражение";
+
+            this.btnAddVariable.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.btnAddVariable.Location = new System.Drawing.Point(20, 60);
+            this.btnAddVariable.Size = new System.Drawing.Size(250, 40);
+            this.btnAddVariable.Text = "Добавить переменную";
+            this.btnAddVariable.Click += new System.EventHandler(this.btnAddVariable_Click);
+
+            this.panelVariables.Location = new System.Drawing.Point(20, 110);
+            this.panelVariables.Size = new System.Drawing.Size(550, 200);
+            this.panelVariables.AutoScroll = true;
+            this.panelVariables.FlowDirection = FlowDirection.TopDown;
+            this.panelVariables.WrapContents = false;
+
+            this.btnCalculate.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.btnCalculate.Location = new System.Drawing.Point(20, 320);
+            this.btnCalculate.Size = new System.Drawing.Size(200, 40);
+            this.btnCalculate.Text = "Вычислить";
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+
+            this.txtResult.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtResult.Location = new System.Drawing.Point(20, 370);
+            this.txtResult.Size = new System.Drawing.Size(500, 29);
+            this.txtResult.ReadOnly = true;
+            this.txtResult.PlaceholderText = "Результат";
+
+            this.tabPageCalc.Controls.Add(this.txtRPN);
+            this.tabPageCalc.Controls.Add(this.btnAddVariable);
+            this.tabPageCalc.Controls.Add(this.panelVariables);
+            this.tabPageCalc.Controls.Add(this.btnCalculate);
+            this.tabPageCalc.Controls.Add(this.txtResult);
+            this.tabPageCalc.Text = "Вычисление ОПЗ";
+            this.tabPageCalc.UseVisualStyleBackColor = true;
+
+            
+            this.txtInfix.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtInfix.Location = new System.Drawing.Point(20, 30);
+            this.txtInfix.Size = new System.Drawing.Size(500, 29);
+            this.txtInfix.PlaceholderText = "Введите инфиксное выражение";
+
+            this.btnConvert.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.btnConvert.Location = new System.Drawing.Point(20, 70);
+            this.btnConvert.Size = new System.Drawing.Size(200, 40);
+            this.btnConvert.Text = "Преобразовать";
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+
+            this.txtRPNResult.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtRPNResult.Location = new System.Drawing.Point(20, 120);
+            this.txtRPNResult.Size = new System.Drawing.Size(500, 29);
+            this.txtRPNResult.ReadOnly = true;
+            this.txtRPNResult.PlaceholderText = "ОПЗ результат";
+
+            this.tabPageConvert.Controls.Add(this.txtInfix);
+            this.tabPageConvert.Controls.Add(this.btnConvert);
+            this.tabPageConvert.Controls.Add(this.txtRPNResult);
+            this.tabPageConvert.Text = "Инфикс → ОПЗ";
+            this.tabPageConvert.UseVisualStyleBackColor = true;
+
+            
+            this.ClientSize = new System.Drawing.Size(640, 470);
+            this.Controls.Add(this.tabControl);
+            this.Text = "RPN Калькулятор";
+            this.tabControl.ResumeLayout(false);
+            this.tabPageCalc.ResumeLayout(false);
+            this.tabPageCalc.PerformLayout();
+            this.tabPageConvert.ResumeLayout(false);
+            this.tabPageConvert.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,237 @@
+namespace RPNCalculator;
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string InfixToRPN(string input)
+        {
+            Dictionary<string, int> precedence = new Dictionary<string, int>
+            {
+                { "+", 1 },
+                { "-", 1 },
+                { "*", 2 },
+                { "/", 2 },
+                { "(", 0 }
+            };
+
+            Stack<string> stack = new Stack<string>();
+            List<string> output = new List<string>();
+
+            string[] tokens = Tokenize(input);
+
+            foreach (string token in tokens)
+            {
+                if (double.TryParse(token, out _) || char.IsLetter(token[0]))
+                {
+                    output.Add(token);
+                }
+                else if (token == "(")
+                {
+                    stack.Push(token);
+                }
+                else if (token == ")")
+                {
+                    while (stack.Count > 0 && stack.Peek() != "(")
+                    {
+                        output.Add(stack.Pop());
+                    }
+                    stack.Pop(); // Удаляем (
+                }
+                else
+                {
+                    while (stack.Count > 0 && precedence[stack.Peek()] >= precedence[token])
+                    {
+                        output.Add(stack.Pop());
+                    }
+                    stack.Push(token);
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                output.Add(stack.Pop());
+            }
+
+            return string.Join(" ", output);
+        }
+
+        private string[] Tokenize(string input)
+        {
+            List<string> tokens = new List<string>();
+            string number = "";
+
+            foreach (char c in input)
+            {
+                if (char.IsWhiteSpace(c)) continue;
+
+                if (char.IsDigit(c) || c == '.')
+                {
+                    number += c;
+                }
+                else
+                {
+                    if (number.Length > 0)
+                    {
+                        tokens.Add(number);
+                        number = "";
+                    }
+
+                    if ("+-*/()".Contains(c))
+                    {
+                        tokens.Add(c.ToString());
+                    }
+                    else if (char.IsLetter(c))
+                    {
+                        tokens.Add(c.ToString());
+                    }
+                    else
+                    {
+                        throw new Exception($"Недопустимый символ: {c}");
+                    }
+                }
+            }
+
+            if (number.Length > 0)
+                tokens.Add(number);
+
+            return tokens.ToArray();
+        }
+
+        private void btnAddVariable_Click(object sender, EventArgs e)
+        {
+            var panel = new FlowLayoutPanel()
+            {
+                FlowDirection = FlowDirection.LeftToRight,
+                AutoSize = true,
+                Margin = new Padding(5)
+            };
+
+            var lblVar = new Label() { Text = "Var:", Width = 40, TextAlign = System.Drawing.ContentAlignment.MiddleLeft };
+            var txtSymbol = new TextBox() { Width = 40, Font = new Font("Segoe UI", 11F) };
+            var lblVal = new Label() { Text = "Value:", Width = 50, TextAlign = System.Drawing.ContentAlignment.MiddleLeft };
+            var txtValue = new TextBox() { Width = 80, Font = new Font("Segoe UI", 11F) };
+
+            var btnRemove = new Button()
+            {
+                Text = "Удалить",
+                AutoSize = true,
+                Font = new Font("Segoe UI", 9F),
+                Margin = new Padding(10, 0, 0, 0)
+            };
+
+            btnRemove.Click += (s, ev) =>
+            {
+                panelVariables.Controls.Remove(panel);
+                panel.Dispose();
+            };
+
+            panel.Controls.Add(lblVar);
+            panel.Controls.Add(txtSymbol);
+            panel.Controls.Add(lblVal);
+            panel.Controls.Add(txtValue);
+            panel.Controls.Add(btnRemove);
+
+            panelVariables.Controls.Add(panel);
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                    string infix = txtInfix.Text;
+                    string rpn = InfixToRPN(infix);
+                    txtRPNResult.Text = rpn;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка преобразования: " + ex.Message);
+                }
+            }
+
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                Dictionary<string, double> variables = new Dictionary<string, double>();
+                foreach (Control ctrl in panelVariables.Controls)
+                {
+                    if (ctrl is FlowLayoutPanel flp && flp.Controls.Count >= 4)
+                    {
+                        string key = ((TextBox)flp.Controls[1]).Text.Trim();
+                        string valStr = ((TextBox)flp.Controls[3]).Text.Trim();
+                        if (double.TryParse(valStr, out double value) && key.Length == 1)
+                        {
+                            variables[key] = value;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Некорректное значение переменной.");
+                            return;
+                        }
+                    }
+                }
+
+                string expression = txtRPN.Text;
+                double result = EvaluateRPN(expression, variables);
+                txtResult.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+
+        private double EvaluateRPN(string expr, Dictionary<string, double> vars)
+        {
+            Stack<double> stack = new Stack<double>();
+            var tokens = expr.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var token in tokens)
+            {
+                if (double.TryParse(token, out double number))
+                {
+                    stack.Push(number);
+                }
+                else if (token.Length == 1 && vars.ContainsKey(token))
+                {
+                    stack.Push(vars[token]);
+                }
+                else if (token == "+" || token == "-" || token == "*" || token == "/")
+                {
+                    if (stack.Count < 2)
+                        throw new Exception("Недостаточно операндов для операции " + token);
+
+                    double b = stack.Pop();
+                    double a = stack.Pop();
+
+                    switch (token)
+                    {
+                        case "+": stack.Push(a + b); break;
+                        case "-": stack.Push(a - b); break;
+                        case "*": stack.Push(a * b); break;
+                        case "/": stack.Push(a / b); break;
+                    }
+                }
+                else
+                {
+                    throw new Exception("Неизвестный токен: " + token);
+                }
+            }
+
+            if (stack.Count != 1)
+                throw new Exception("Ошибка в выражении. Осталось несколько элементов в стеке.");
+
+            return stack.Pop();
+        }
+}
+
